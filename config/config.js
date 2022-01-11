acae63761fb94629c0ffb3ad59a6eb4a5ed46463
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-01-11 18:16:41 +0530

    :sparkles: added configs

---
+++ config/config.js
@@ -0,0 +1,11 @@
+// MongoDB configs
+const Configuration = {
+  Port: process.env.PORT || 3000,
+  db_url: process.env.atlas_url,
+  db_config: {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+};
+
+module.exports = Configuration;
