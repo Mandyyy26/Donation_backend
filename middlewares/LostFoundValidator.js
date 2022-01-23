83a599c65ce58f114ac1f37ac2204c5bf9795dfc
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-01-24 00:01:54 +0530

    :sparkles: removed file necessary feature in Lost Found section

--- middlewares/LostFoundValidator.js
+++ middlewares/LostFoundValidator.js
@@ -15,14 +15,6 @@ const LostFoundValidSchema = Joi.object({
 const ValidateLostFound = (req, res, next) => {
   let files = [];
 
-  const isNewProduct = req.route.path === "/create-new-lost-found-product";
-
-  // If there are no files, send the error
-  if (isNewProduct && req.files?.length === 0)
-    return res
-      .status(400)
-      .send({ message: "Atleast one product image is required." });
-
   // If req.files is not empty, take the files and store it in an array
   if (req.files?.length > 0) files = req.files.map((file) => file.buffer);
 
