f5b6d07abd9469d09a7461b1bdf6a474ebc52738
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-01-31 13:25:08 +0530

    :racehorse: Integrated `sockets` for Chat

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ require("dotenv").config();
 const config = require("./config/config");
 const messages = require("./config/messages");
 
+// Socket Setup
+const http = require("http");
+const socketio = require("socket.io");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/Socket");
+
 // Routes imports
 const BuySellItem = require("./routes/BuySellItem");
 const Chats = require("./routes/Chats");
@@ -26,11 +36,54 @@ mongoose
 // Express app initialization
 const app = express();
 
+// Configuring Express to use static files
+app.use(express.static(path.join(__dirname, "/public")));
+
 // Configuring Express app for production
 require("./config/Production")(app);
 
-// Configuring Express to use static files
-app.use(express.static(path.join(__dirname, "/public")));
+// Wrapping Express app with Socket.io
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Socket listeners
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    console.log(getRoomUsers(user.room));
+
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("message", msg);
+  });
+
+  socket.on("type-update", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    io.to(user.room).emit("type-update-emitter", msg);
+  });
+
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
 
 // Add routes
 app.use(process.env.apiVersion + process.env.buysell, BuySellItem);
@@ -41,8 +94,12 @@ app.use(process.env.apiVersion + process.env.raisedhands, RaisedHands);
 app.use(process.env.apiVersion + process.env.requirements, Requirements);
 app.use(process.env.apiVersion + process.env.auth, User);
 
-// App listening on port
-app.listen(config.Port, () =>
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname + "/views/404.html"));
+});
+
+// Server listening on port
+server.listen(config.Port, () =>
   console.log(
     `Mode = ${process.env.NODE_ENV} and Listening on ${config.Port}..`
   )
