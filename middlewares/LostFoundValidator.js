3a4310ad6276d61f255835c9db12582b9f5614b4
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-02-01 00:08:16 +0530

    :art: controllers/middleware cleanup

--- middlewares/LostFoundValidator.js
+++ middlewares/LostFoundValidator.js
@@ -1,17 +1,3 @@
-// Import the required modules
-const Joi = require("joi");
-
-// Exporting Register Schema
-const LostFoundValidSchema = Joi.object({
-  name: Joi.string(),
-  description: Joi.string(),
-  files: Joi.array(),
-  related_questions: Joi.array(),
-  user_details: Joi.object(),
-  to_be_deleted: Joi.array(),
-  product_id: Joi.string(),
-}).options({ allowUnknown: true });
-
 const ValidateLostFound = (req, res, next) => {
   let files = [];
 
@@ -19,11 +5,6 @@ const ValidateLostFound = (req, res, next) => {
   if (req.files?.length > 0) files = req.files.map((file) => file.buffer);
 
   let newBody = { ...req.body, files: files };
-
-  const result = LostFoundValidSchema.validate(req.body);
-
-  if (result.error)
-    return res.status(400).send({ message: result.error.details[0].message });
 
   req.body = newBody;
 
