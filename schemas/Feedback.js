c5798e9ae63179d0b2931401e3134ebf323c48f2
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-02-14 14:29:34 +0530

    :art: Indentation fixed

--- schemas/Feedback.js
+++ schemas/Feedback.js
@@ -3,16 +3,16 @@ const mongoose = require("mongoose");
 
 // Feedback Schema
 const feedbackSchema = new mongoose.Schema({
-	feedback: {
-		type: String,
-		required: true,
-	},
-	owner_id: {
-		type: mongoose.Schema.ObjectId,
-		ref: "users",
-		required: true,
-	},
-	feedback_datetime: { type: Date, default: Date.now, required: true },
+  feedback: {
+    type: String,
+    required: true,
+  },
+  owner_id: {
+    type: mongoose.Schema.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  feedback_datetime: { type: Date, default: Date.now, required: true },
 });
 
 // Exporting the Feedback schema
