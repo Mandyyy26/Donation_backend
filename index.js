892b9a0b5cbf087d340155846365971c8d55a9a6
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-02-16 13:04:22 +0530

    :hammer: Created seperate function to create indexes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 // Importing Packages
 const express = require("express");
+const favicon = require("serve-favicon");
 const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config();
 
 // Utils/Configs imports
 const config = require("./config/config");
+const { CreateIndexes } = require("./config/ModelIndexes");
 const messages = require("./config/messages");
 
 // Socket Setup
@@ -23,35 +25,21 @@ const RaisedHands = require("./routes/RaisedHands");
 const Requirements = require("./routes/Requirements");
 const User = require("./routes/Users");
 
-// Model Imports
-const { buySellItems } = require("./models/BuySellItem");
-const { lostFoundItems } = require("./models/LostFoundItem");
-
 // Connect to MongoDB
 mongoose
   .connect(config.db_url, config.db_config)
-  .then(() => console.log(`Connected to ${process.env.DB_Name} Mongo DB...`))
+  .then(() => {
+    console.log(`Connected to ${process.env.DB_Name} Mongo DB...`);
+    CreateIndexes();
+  })
   .catch(error => console.error(messages.serverError, error));
-
-// Create Indexes for Models
-buySellItems.collection.createIndex({
-  name: "text",
-  description: "text",
-});
-
-lostFoundItems.collection.createIndex({
-  name: "text",
-  description: "text",
-  brand: "text",
-  category: "text",
-  color: "text",
-});
 
 // Express app initialization
 const app = express();
 
 // Configuring Express to use static files
 app.use(express.static(path.join(__dirname, "/public")));
+app.use(favicon(__dirname + "/favicon.ico"));
 
 // Configuring Express app for production
 require("./config/Production")(app);
