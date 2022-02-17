f1ec987078e452f55855ed0b44777793c279b284
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-02-17 17:04:56 +0530

    :sparkles: added new fields to buy-sell schema

--- schemas/BuySellItem.js
+++ schemas/BuySellItem.js
@@ -15,6 +15,26 @@ const buySellItemSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  brand: {
+    type: String,
+    default: "",
+  },
+  category: {
+    type: String,
+    default: "",
+  },
+  color: {
+    type: String,
+    default: "",
+  },
+  bought_datetime: {
+    type: Date,
+    default: null,
+  },
+  warranty_till: {
+    type: Date,
+    default: null,
+  },
   posted_on: { type: Date, default: Date.now },
   files: {
     type: Array,
